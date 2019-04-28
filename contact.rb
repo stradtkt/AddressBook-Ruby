9a09dc2818c8d28167c8aea88defcabf257cec9a
Kevin <stradtkt22@gmail.com>
2019-04-28 00:21:03 -0400

    Added the rest of contact

--- contact.rb
+++ contact.rb
@@ -13,6 +13,22 @@ class Contact
     @last_name
   end
 
+  def first_last
+    first_name + " " + last_name
+  end
+
+  def last_first
+    last_first = last_name
+    last_first += ", "
+    last_first += first_name
+    if !@middle_name.nil?
+      last_first += " "
+      last_first += middle_name.slice(0, 1)
+      last_first += "."
+    end
+    last_first
+  end
+
   def full_name
     full_name = first_name
     if !@middle_name.nil?
@@ -23,6 +39,21 @@ class Contact
     full_name += last_name
     full_name
   end
+
+  def to_s(format = "full_name")
+    case format
+    when 'full_name'
+      full_name
+    when 'last_first'
+      last_first
+    when 'first'
+      first_name
+    when 'last'
+      last_name
+    else
+      first_last
+    end
+  end
 end
 
 
@@ -31,4 +62,5 @@ kevin.first_name = "Kevin"
 kevin.last_name = "Stradtman"
 kevin.middle_name = "Thomas"
 puts kevin.full_name
+puts kevin.first_name
 
