a615dc1d1cf0f9444d992636e7284fd393d4246f
Kevin <stradtkt22@gmail.com>
2019-04-28 00:05:31 -0400

    Initial Commit

---
+++ contact.rb
@@ -0,0 +1,34 @@
+class Contact
+  attr_writer :first_name, :middle_name, :last_name
+
+  def first_name
+    @first_name
+  end
+
+  def middle_name
+    @middle_name
+  end
+
+  def last_name
+    @last_name
+  end
+
+  def full_name
+    full_name = first_name
+    if !@middle_name.nil?
+      full_name += " "
+      full_name += middle_name
+    end
+    full_name += " "
+    full_name += last_name
+    full_name
+  end
+end
+
+
+kevin = Contact.new
+kevin.first_name = "Kevin"
+kevin.last_name = "Stradtman"
+kevin.middle_name = "Thomas"
+puts kevin.full_name
+
